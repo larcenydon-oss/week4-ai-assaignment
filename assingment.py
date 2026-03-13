0fd7dab6dbb1c452b94c7bf8abf46a358d23abfd
Don kamau <larcenydon@gmail.com>
2026-03-13 22:41:57 +0300

    assingment.py

--- assingment.py
+++ assingment.py
@@ -1,1 +1,15 @@
+#Question 4
+print('hello AI students')
+#Question 5
+name = "John"
+age = 34
+city = "Nairobi"
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"city: {city}")
 
+#question 6
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+print(languages[0])
+#question 7
+student = {'name': 'John', 'age': 34, 'course': 'AI'}
