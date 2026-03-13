96d4325c01485c97a0515a07c770fc5d06d25ce5
Don kamau <larcenydon@gmail.com>
2026-03-13 22:52:50 +0300

    Update assingment.py

--- assingment.py
+++ assingment.py
@@ -7,9 +7,12 @@ city = "Nairobi"
 print(f"name: {name}")
 print(f"age: {age}")
 print(f"city: {city}")
-
+john
+34
+nairobi
 #question 6
 languages = ["Python", "Java", "C++", "JavaScript", "Go"]
 print(languages[0])
+python
 #question 7
 student = {'name': 'John', 'age': 34, 'course': 'AI'}
